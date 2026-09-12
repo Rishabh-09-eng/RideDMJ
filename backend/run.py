a22ab5cf59a3168feb23463273102fc5239e777d
Rohit Bellikatti <dayanandbellikatti@gmail.com>
2026-09-12 21:53:18 +0530

    feat: add initial user and bookings modules

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from payments.routes import router as payment_router
+from database import engine
+from fastapi import FastAPI
 
 app = FastAPI()
 
+from .users.routes import router
+ 
 app.include_router(payment_router)
