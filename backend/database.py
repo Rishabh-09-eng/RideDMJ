6de383d18fdce7b8657174ef247cea3fc0f359d0
Rohit Bellikatti <dayanandbellikatti@gmail.com>
2026-09-13 02:31:56 +0530

    refactor: reorganize imports and improve sessionLocal definition

--- backend/database.py
+++ backend/database.py
@@ -1,16 +1,27 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 import os
+
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
 
 db_url = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 engine = create_engine(db_url)
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+sessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
 Base = declarative_base()
+
 
 def get_db():
     db = sessionLocal()
+
     try:
         yield db
     finally:
