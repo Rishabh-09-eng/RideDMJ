db717ea549fa62df467ab2d39b5e7cc36f3d2519
Rishabh <rishabhkachhot1409@gmail.com>
2026-09-12 17:34:30 +0530

    redirect after login

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,5 @@
 import Hero from "@/component/Hero";
-import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/login");
-  return (
-    <Hero />
-  );
+  return <Hero />;
 }
