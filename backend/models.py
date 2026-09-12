ee574ed2159faae716a47ecec55ddfcbdb3edc06
Rohit Bellikatti <dayanandbellikatti@gmail.com>
2026-09-13 01:23:38 +0530

    refactor: clean up import statements in models and run files

--- backend/models.py
+++ backend/models.py
@@ -1,4 +1,4 @@
-from .database import Base
+from database import Base
 from sqlalchemy import Column, Integer, Date, DateTime, Time, ForeignKey, String
 from sqlalchemy.dialects.postgresql import UUID
 
--- backend/run.py
+++ backend/run.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
-from payments.routes import router as payment_router
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from payments.routes import router as payment_router
+from database import Base
+import models
+
+
 app = FastAPI()
- 
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],
-
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 app.include_router(payment_router)
