f54060a2a4d112eef7debb04cb13b4f1e541b038
Rishabh <rishabhkachhot1409@gmail.com>
2026-09-12 16:34:29 +0530

    added login

--- frontend/component/Hero.js
+++ frontend/component/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -12,18 +13,23 @@ const Hero = () => {
         priority
       />
       <div className="absolute left-12 top-1/2 -translate-y-1/2 text-white">
-        <h1 className="text-5xl font-bold">
+        <h1 className="text-5xl font-bold text-white/90">
           Travel Smarter with RideDMJ
         </h1>
 
-        <p className="mt-4 max-w-lg text-lg">
+        <p className="mt-4 max-w-lg text-lg text-gray-400">
           Your campus transportation, made simple.
           Check bus schedules, manage bookings, and travel with ease.
         </p>
 
-        <button className="mt-6 rounded-lg bg-green-500 px-6 py-3 font-semibold text-white hover:bg-green-600">
-          Explore Bus Schedules
-        </button>
+        <div className="flex gap-10">
+            <Link href=""><button className="mt-6 rounded-lg bg-green-500 px-6 py-3 font-semibold text-white hover:bg-green-600">
+              Explore Bus Schedules
+            </button></Link>
+            <Link href="/book-ticket"><button className="mt-6 rounded-lg bg-green-500 px-6 py-3 font-semibold text-white hover:bg-green-600">
+              Book Ticket
+            </button></Link>
+        </div>
       </div>
     </section>
   );
