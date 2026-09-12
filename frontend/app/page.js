8f6e8166277b17ca43ec9fa8e29444e3e1ff5cfc
Rishabh <rishabhkachhot1409@gmail.com>
2026-09-12 15:39:08 +0530

    Update hero section

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,7 +1,7 @@
-import Image from "next/image";
+import Hero from "@/component/Hero";
 
 export default function Home() {
   return (
-    <div></div>
+    <Hero />
   );
 }
--- frontend/component/Navbar.js
+++ frontend/component/Navbar.js
@@ -1,28 +1,29 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 import {UserRound} from 'lucide-react'
 
 const Navbar = () => {
   return (
     <>
-        <nav className='flex justify-between items-center px-8 py-4 gap-10'>
-            <div className='flex justify-between items-center'>
+        <nav className='flex justify-between items-center px-8 py-4 gap-10 bg-[#334155]'>
+            <Link href="/"><div className='flex justify-between items-center'>
                 <Image className='' width={50} height={50} src="/logo.svg" alt="Logo" />
                 <div>
-                    <span className='text-black text-2xl'>Ride</span>
-                    <span className='text-green-700 text-2xl'>DMJ</span>
+                    <span className='text-white text-2xl'>Ride</span>
+                    <span className='text-green-400 text-2xl'>DMJ</span>
                 </div>
-            </div>
+            </div></Link>
             <ul className='flex gap-10'>
-                <li className='font-bold'>Home</li>
-                <li className='font-bold'>Bus Schedule</li>
-                <li className='font-bold'>My Booking</li>
-                <li className='font-bold'>Contact</li>
+                <Link href="/"><li className='text-white font-bold'>Home</li></Link>
+                <Link href=""><li className='text-white font-bold'>Bus Schedule</li></Link>
+                <Link href=""><li className='text-white font-bold'>My Booking</li></Link>
+                <Link href=""><li className='text-white font-bold'>Contact</li></Link>
             </ul>
             <div>
                 <UserRound
                   size={28}
-                  className="text-gray-700 cursor-pointer"
+                  className="text-white cursor-pointer"
                 />
             </div>
         </nav>
