cc4ae9d91ba3f9950fc6506a42cceaf3500d0d18
Rishabh <rishabhkachhot1409@gmail.com>
2026-09-13 12:43:35 +0530

    Configure production frontend CORS

--- backend/run.py
+++ backend/run.py
@@ -35,6 +35,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "http://172.16.0.2:3000",
+        "https://ridedmj-rosy.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
